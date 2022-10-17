46c100bc10c04770f3f0176e7ee4d14646b8c1b7
owen-hwlee <hwolee@connect.ust.hk>
2022-10-18 01:53:59 +0800

    Implemented App.java

--- src/main/java/hk/ust/comp3021/gui/App.java
+++ src/main/java/hk/ust/comp3021/gui/App.java
@@ -1,17 +1,24 @@
 package hk.ust.comp3021.gui;
 
+import hk.ust.comp3021.game.GameState;
 import hk.ust.comp3021.gui.component.maplist.MapEvent;
 import hk.ust.comp3021.gui.scene.game.ExitEvent;
 import hk.ust.comp3021.gui.scene.game.GameScene;
 import hk.ust.comp3021.gui.scene.start.StartScene;
+import hk.ust.comp3021.gui.utils.Message;
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 /**
  * The JavaFX application that launches the game.
  */
 public class App extends Application {
     private Stage primaryStage;
+
+    private StartScene startScene;
 
     /**
      * Set up the primary stage and show the {@link StartScene}.
@@ -25,19 +32,31 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         this.primaryStage = primaryStage;
-        primaryStage.setTitle("Sokoban Game - COMP3021 2022Fall");
+        this.primaryStage.setTitle("Sokoban Game - COMP3021 2022Fall");
 
-        // TODO
+        // DONE
+        this.startScene = new StartScene();
+        this.primaryStage.setScene(this.startScene);
+        this.primaryStage.show();
     }
 
     /**
      * Event handler for opening a map.
-     * Swith to the {@link GameScene} in the {@link this#primaryStage}.
+     * Switch to the {@link GameScene} in the {@link this#primaryStage}.
      *
      * @param event The event data related to the map being opened.
      */
     public void onOpenMap(MapEvent event) {
-        // TODO
+        // DONE
+        this.primaryStage.hide();
+        Path mapPath = event.getModel().file().toAbsolutePath();
+        try {
+            this.primaryStage.setScene(new GameScene(new GameState(event.getModel().gameMap())));
+            this.primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Message.error("Failed to open game map.", "Failed to open game map at %s".formatted(mapPath));
+        }
     }
 
     /**
@@ -47,6 +66,9 @@ public class App extends Application {
      * @param event The event data related to exiting the game.
      */
     public void onExitGame(ExitEvent event) {
-        // TODO
+        // DONE
+        this.primaryStage.hide();
+        this.primaryStage.setScene(this.startScene);
+        this.primaryStage.show();
     }
 }
