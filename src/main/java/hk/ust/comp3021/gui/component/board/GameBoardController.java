c6017b28a29a6de345afc7d32d16fc1f01c129a2
owen-hwlee <hwolee@connect.ust.hk>
2022-10-23 10:58:54 +0800

    Implemented GameBoardController::render

--- src/main/java/hk/ust/comp3021/gui/component/board/GameBoardController.java
+++ src/main/java/hk/ust/comp3021/gui/component/board/GameBoardController.java
@@ -1,8 +1,11 @@
 package hk.ust.comp3021.gui.component.board;
 
+import hk.ust.comp3021.entities.*;
 import hk.ust.comp3021.game.GameState;
+import hk.ust.comp3021.game.Position;
 import hk.ust.comp3021.game.RenderingEngine;
 import hk.ust.comp3021.gui.utils.Message;
+import hk.ust.comp3021.gui.utils.Resource;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -10,7 +13,9 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 /**
@@ -36,7 +41,51 @@ public class GameBoardController implements RenderingEngine, Initializable {
      */
     @Override
     public void render(@NotNull GameState state) {
-        // TODO
+        // DONE
+
+        // Add undoQuota to board
+        this.undoQuota.setText(
+                "Undo quota: " +
+                        (state.getUndoQuota().isPresent() ? state.getUndoQuota().get().toString() : "unlimited")
+        );
+
+        // Re-render the map
+        try {
+            this.map.getChildren().clear();
+            for (int rowIndex = 0; rowIndex < state.getMapMaxHeight(); ++rowIndex) {
+                for (int columnIndex = 0; columnIndex < state.getMapMaxWidth(); ++columnIndex) {
+                    Cell cell = new Cell();
+                    Entity entity = state.getEntity(Position.of(columnIndex, rowIndex));
+                    if (Objects.nonNull(entity)) {
+                        URL imageUrl = switch (entity) {
+                            case Player player -> Resource.getPlayerImageURL(player.getId());
+                            case Box box -> {
+                                // Check if box is on destination
+                                if (state.getDestinations().contains(Position.of(columnIndex, rowIndex))) {
+                                    cell.getController().markAtDestination();
+                                }
+                                yield Resource.getBoxImageURL(box.getPlayerId());
+                            }
+                            case Wall ignored -> Resource.getWallImageURL();
+                            case Empty ignored -> {
+                                // Check if cell is a destination
+                                if (state.getDestinations().contains(Position.of(columnIndex, rowIndex))) {
+                                    yield Resource.getDestinationImageURL();
+                                } else {
+                                    yield Resource.getEmptyImageURL();
+                                }
+                            }
+                        };
+                        cell.getController().setImage(imageUrl);
+                    }
+                    int finalColumnIndex = columnIndex;
+                    int finalRowIndex = rowIndex;
+                    Platform.runLater(() -> this.map.add(cell, finalColumnIndex, finalRowIndex));
+                }
+            }
+        } catch (IOException e) {
+            Message.error("Render error", "Unable to render game board cells.");
+        }
     }
 
     /**
